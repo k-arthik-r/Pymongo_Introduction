c64571bd7e1a8381868eda6c971a990ded07dd32
Karthik R <karthikclgid@gmail.com>
2023-06-09 23:12:35 +0530

    add sort.py

---
+++ sort.py
@@ -0,0 +1,25 @@
+import pymongo
+
+client = pymongo.MongoClient('localhost', 27017)
+db = client["student"]
+collection = db["CSE"]
+
+
+# it will arrange the names in ascending order
+query = collection.find().sort("name")
+for x in query:
+    print(x)
+
+
+# it will arrange the names in ascending order
+queryb = collection.find().sort("name", 1)
+for x in queryb:
+    print(x)
+
+
+# it will arrange the names in descending order
+querya = collection.find().sort("name", -1)
+for x in querya:
+    print(x)
+
+# Ascending = 1 and Descnding = -1
